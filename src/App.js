be419dce8ac1789721bd0e18977d85f0ac2e077b
GuidoToritto <guidotoritto>
2022-12-07 23:49:12 +0100

    Agregando contenido

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
-import Navbar from './components/navegacion/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';import Navbar from './components/navegacion/Navbar';
 import Inicio from './components/paginas/Inicio';
 import Contenido from './components/paginas/Contenido';
 import Items from './components/paginas/Items';
 
 function App() {
   return (
-    <div className="App">
-     <Router>
-      <Navbar/>
-     </Router>
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route exact path='/' element={ <Inicio/>}/>
+      <Route path='Contenido' element={<Contenido/>}/>
+      <Route path='Items' element ={<Items/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/paginas/Contenido.js
+++ src/components/paginas/Contenido.js
@@ -1,10 +1,115 @@
 import React from 'react'
+import shoe from '../../../src/imagenes/Contenido/1.png';
+import shoe2 from '../../../src/imagenes/Contenido/2.png';
+import shoe3 from '../../../src/imagenes/Contenido/3.png';
+import shoe4 from '../../../src/imagenes/Contenido/4.png';
 
 export const Contenido = () => {
   return (
-    <div>
-        <h1>Esta es la página de Contenido</h1>
-    </div>
+    <>
+      <h1 className='title'>Productos</h1>
+      
+      <div className='productos'>
+          <div className='producto'>
+              <a href='#'>
+                  <div className='producto_img'>
+                    <img src={shoe}/>
+                  </div>
+                </a>
+                <div className='producto_footer'>
+                  <h1>New Balance 550</h1>
+                  <p> White-Red</p>
+                  <p className='price'> $149,99 </p>
+                </div>
+
+                <div className='buttom'>
+                  <button className='btn'>
+                    Añadir al carrito
+                  </button>
+                  <a href='#' className='btn'>Vista</a>
+                <div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className='producto'>
+              <a href='#'>
+                  <div className='producto_img'>
+                    <img src={shoe2}/>
+                  </div>
+                </a>
+                <div className='producto_footer'>
+                  <h1>Nike Dunk Low</h1>
+                  <p> Iron Grey-Green</p>
+                  <p className='price'> $119,99 </p>
+                </div>
+                
+                <div className='buttom'>
+                  <button className='btn'>
+                    Añadir al carrito
+                  </button>
+                  <a href='#' className='btn'>Vista</a>
+                <div>
+                
+              </div>
+            </div>
+          </div>
+        
+        
+      </div>
+
+      <div className='productos'>
+          <div className='producto'>
+              <a href='#'>
+                  <div className='producto_img'>
+                    <img src={shoe3}/>
+                  </div>
+                </a>
+                <div className='producto_footer'>
+                  <h1>Vans Old Skool MTE</h1>
+                  <p> Black-White</p>
+                  <p className='price'> $99,99 </p>
+                </div>
+
+                <div className='buttom'>
+                  <button className='btn'>
+                    Añadir al carrito
+                  </button>
+                  <a href='#' className='btn'>Vista</a>
+                <div>
+
+              </div>
+            </div>
+          </div>
+
+          <div className='producto'>
+              <a href='#'>
+                  <div className='producto_img'>
+                    <img src={shoe4}/>
+                  </div>
+                </a>
+                <div className='producto_footer'>
+                  <h1>Converse Chuck Taylor</h1>
+                  <p> White-Grey</p>
+                  <p className='price'> $59,99 </p>
+                </div>
+                
+                <div className='buttom'>
+                  <button className='btn'>
+                    Añadir al carrito
+                  </button>
+                  <a href='#' className='btn'>Vista</a>
+                <div>
+                
+              </div>
+            </div>
+          </div>
+        
+        
+      </div>
+
+    </>
   )
 }
 
